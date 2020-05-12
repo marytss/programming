196e17c84079c6d71376a93cbf48c6cbcb7726ff
Мария Царькова <64022487+marytss@users.noreply.github.com>
2020-05-12 13:48:25 +0300

    Add files via upload

---
+++ мили - километры.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Мили___километры
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double m = Convert.ToDouble(textBox1.Text);
+            double k;
+            k = m * 1.609;
+            textBox2.Text = k.ToString();
+        }
+    }
+}
